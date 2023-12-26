1cc9792e06fd75c8204678b2856b92d93ad35b87
Raphaël Chiche <raphael.ch2004@gmail.com>
2023-12-26 17:23:18 +0100

    ✨ NEW arrow up scroll to the top

--- assets/javascript/assets.js
+++ assets/javascript/assets.js
@@ -1,18 +1,32 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    var circle = document.querySelector('.progress__value');
-    var radius = circle.r.baseVal.value;
-    var circumference = radius * 2 * Math.PI;
-    circle.style.strokeDasharray = `${circumference} ${circumference}`;
-    circle.style.strokeDashoffset = `${circumference}`;
+document.addEventListener("DOMContentLoaded", (event) => {
+  var circle = document.querySelector(".progress__value");
+  var radius = circle.r.baseVal.value;
+  var circumference = radius * 2 * Math.PI;
+  circle.style.strokeDasharray = `${circumference} ${circumference}`;
+  circle.style.strokeDashoffset = `${circumference}`;
 
-    function setProgress(percent) {
-        var offset = circumference - percent / 100 * circumference;
-        circle.style.strokeDashoffset = offset;
+  function setProgress(percent) {
+    var offset = circumference - (percent / 100) * circumference;
+    circle.style.strokeDashoffset = offset;
+  }
+
+  window.addEventListener("scroll", function () {
+    var max = document.body.scrollHeight - window.innerHeight;
+    var percent = (window.pageYOffset / max) * 100;
+    setProgress(percent);
+
+    var arrowUp = document.querySelector("#arrowUp");
+    if (window.scrollY > 150) {
+      arrowUp.style.opacity = "1";
+      arrowUp.style.transform = "translateY(0)";
+    } else {
+      arrowUp.style.opacity = "0";
+      arrowUp.style.transform = "translateY(20px)";
     }
+  });
+});
 
-    window.addEventListener('scroll', function() {
-        var max = document.body.scrollHeight - window.innerHeight;
-        var percent = (window.pageYOffset / max) * 100;
-        setProgress(percent);
-    });
-}); 
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
