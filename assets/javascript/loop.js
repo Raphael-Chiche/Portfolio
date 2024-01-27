a7f6d169217f74c4cff1f2dfcb54fcf20147103f
Raphaël Chiche <raphael.ch2004@gmail.com>
2024-01-27 13:06:09 +0100

    Add styles for project descriptions and animation for stars

--- assets/javascript/loop.js
+++ assets/javascript/loop.js
@@ -1,2 +1,9 @@
 var copy = document.querySelector(".logos-slide").cloneNode(true);
-      document.querySelector(".logos").appendChild(copy);
+document.querySelector(".logos").appendChild(copy);
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  var petitProjets = document.getElementById("petitProjets");
+  petitProjets.addEventListener("click", function () {
+    window.location.href = "petitProjets.html";
+  });
+});
